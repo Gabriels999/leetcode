6063b650ec46c2a9fad40f73dc31802fc91e265a
Gabriel Silva Santos <gabriel.santos@buser.com.br>
2023-01-11 18:54:58 -0300

    finishes calculate digit

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 - Easy:
     1. <a href='https://leetcode.com/problems/palindrome-number/'>Numeros Palindromos</a> (Easy - JS)
     2. <a href='https://leetcode.com/problems/roman-to-integer/'>Roman to Integer</a> (Easy - JS)
-    2. <a href='https://leetcode.com/problems/plus-one/'>Plus One</a> (Easy - JS)
+    3. <a href='https://leetcode.com/problems/plus-one/'>Plus One</a> (Easy - JS)
+    4. <a href='https://leetcode.com/problems/calculate-digit-sum-of-a-string/'>Calculate Digit Sum of a String</a> (Easy - JS)
--- calculate_digit_sum_of_string.js
+++ calculate_digit_sum_of_string.js
@@ -1,31 +1,26 @@
 var digitSum = function (s, k) {
-    // const compareIndexWithK = function (index, k) {
-    //     let newIndex = ''
-    //     for (i of index) {
-    //         newIndex += i
-    //     }
-    //     return newIndex
-    // }
     const divideStringGroups = function (s, k) {
+        if (typeof (s) == 'object') {
+            s = s.join('').replace(',', '')
+        }
         let bigArray = []
         let count = k
         let actualPart = '' //devo trocar isso para list ?
         for (let i = 0; i < s.length; i++) {
-            if (s[i].length == k) {
-                actualPart += s[i]
-                count--
-                if ((count == 0) || i == (s.length - 1)) {
-                    bigArray.push(actualPart)
-                    actualPart = ''
-                    count = k
-                }
-            } else {
-                compareIndexWithK(s[i], k)
+            actualPart += s[i]
+            count--
+            if ((count == 0) || i == (s.length - 1)) {
+                bigArray.push(actualPart)
+                actualPart = ''
+                count = k
             }
         }
         return bigArray
     }
-    const sumListOfDigits = function (arr) {
+    const sumListOfDigits = function (arr, k) {
+        if (arr.join('').replace(',', '').length <= k) {
+            return arr
+        }
         let newArray = []
         for (let i = 0; i < arr.length; i++) {
             let total = 0
@@ -45,11 +40,13 @@ var digitSum = function (s, k) {
     }
     while (s.length > k) {
         const dividedGroups = divideStringGroups(s, k)
-        s = sumListOfDigits(dividedGroups)
+        s = sumListOfDigits(dividedGroups, k)
+        s = s.join('').replace(',', '')
     }
     return sumDigitsFinalRound(s)
 };
 
+console.log(digitSum(s = "55179381572", k = 4))
 console.log(digitSum(s = "01234567890", k = 2))
 console.log(digitSum(s = "11111222223", k = 3))
 console.log(digitSum(s = "00000000", k = 3))
